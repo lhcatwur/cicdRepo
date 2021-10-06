c90ecb656ef51abc1bbf75baed89dcd4d31f8239
lhcamilo <lhcamilo@gmail.com>
2021-10-06 09:50:54 +0200

    Add temperature variable

--- example.py
+++ example.py
@@ -3,5 +3,12 @@
 # File              : example.py
 # Date              : 06.10.2021
 # Last Modified Date: 06.10.2021
+# -*- coding: utf-8 -*-
+# File              : example.py
+# Date              : 06.10.2021
+# Last Modified Date: 06.10.2021
+temperature = 10
+
 def print_temperature():
+
     print(temperature)
