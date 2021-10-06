3c7a51091c96a9cf5ddfea5651b0ea93601e07a2
lhcamilo <lhcamilo@gmail.com>
2021-10-06 09:30:40 +0200

    Create example.py with a print temperature function

---
+++ example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : example.py
+# Date              : 06.10.2021
+# Last Modified Date: 06.10.2021
+def print_temperature():
+    print(temperature)
